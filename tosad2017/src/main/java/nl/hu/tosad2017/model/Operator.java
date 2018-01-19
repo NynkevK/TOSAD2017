4a93f13bf4f462707ccf849838ee4aaac16dda5c
Mastahpiece <m_s_abdoel@hotmail.com>
2018-01-19 12:20:40 +0100

    package namen :D

---
+++ tosad2017/src/main/java/nl/hu/tosad2017/model/Operator.java
@@ -0,0 +1,13 @@
+package main.java.nl.hu.tosad2017.model;
+
+public class Operator {
+    private String operaterName;
+
+    public String getOperaterName() {
+        return operaterName;
+    }
+
+    public void setOperaterName(String operaterName) {
+        this.operaterName = operaterName;
+    }
+}
---
+++ tosad2017/src/main/java/nl/hu/tosad2017/model/RangeRule.java
@@ -0,0 +1,14 @@
+package main.java.nl.hu.tosad2017.model;
+
+public class RangeRule extends BusinessRule {
+    private int minValue;
+    private int maxValue;
+
+    public int getMinValue() { return minValue; }
+
+    public void setMinValue(int minValue) { this.minValue = minValue; }
+
+    public int getMaxValue() { return maxValue; }
+
+    public void setMaxValue(int maxValue) { this.maxValue = maxValue; }
+}
