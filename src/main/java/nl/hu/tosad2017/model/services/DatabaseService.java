6354d1b48bc824a38568b40738f5718b270cbf4a
Meimeifeng <tickets_to_life@hotmail.com>
2018-01-28 18:32:43 +0100

    database other tables working(?) resources

--- src/main/java/nl/hu/tosad2017/model/services/DatabaseService.java
+++ src/main/java/nl/hu/tosad2017/model/services/DatabaseService.java
@@ -1,4 +1,13 @@
 package nl.hu.tosad2017.model.services;
 
+import nl.hu.tosad2017.model.model.DataInfo;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public class DatabaseService {
+
+    public List<DataInfo> getAllDataInfo() throws SQLException {
+        return  null;
+    }
 }
--- src/main/java/nl/hu/tosad2017/webservices/DatabaseResource.java
+++ src/main/java/nl/hu/tosad2017/webservices/DatabaseResource.java
@@ -2,10 +2,46 @@ package nl.hu.tosad2017.webservices;
 
 import nl.hu.tosad2017.model.services.DatabaseService;
 import nl.hu.tosad2017.model.services.ServiceProvider;
+import nl.hu.tosad2017.model.model.DataInfo;
 
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import java.sql.SQLException;
 
-    public class DatabaseResource {
+@Path( "/datainfo")
+public class DatabaseResource {
         // initialise service
         DatabaseService databaseService = ServiceProvider.getDatabaseService();
+
+    @GET
+    @Produces("application/json")
+    public String getAllRangeRules() throws SQLException {
+        // logging for Heroku application server
+        System.out.println(".. executing DataInfo Resource (GET) for all");
+
+        JsonArrayBuilder jab = Json.createArrayBuilder();
+
+        //Add each datainfo to a json object
+        for (DataInfo d : databaseService.getAllDataInfo()) {
+            JsonObjectBuilder job = Json.createObjectBuilder();
+            job.add("tablename", d.getTableName());
+            job.add("columnname", d.getColumnName());
+            job.add("datatype", d.getDataType());
+            jab.add(job);
+        }
+
+        if (jab == null ) {
+            throw new WebApplicationException("No rues found!");
+        }
+
+        JsonArray array = jab.build();
+        return array.toString();
+    }
 
 }
