58dda77d02fa0920767b0e7c29756bf09d99fe8f
Meimeifeng <tickets_to_life@hotmail.com>
2018-01-28 19:15:25 +0100

    update databaseService

--- src/main/java/nl/hu/tosad2017/model/services/DatabaseService.java
+++ src/main/java/nl/hu/tosad2017/model/services/DatabaseService.java
@@ -1,13 +1,16 @@
 package nl.hu.tosad2017.model.services;
 
 import nl.hu.tosad2017.model.model.DataInfo;
+import nl.hu.tosad2017.persistence.data.DatabaseDAO;
 
 import java.sql.SQLException;
 import java.util.List;
 
 public class DatabaseService {
+    DatabaseDAO dataDAO = new  DatabaseDAO();
 
     public List<DataInfo> getAllDataInfo() throws SQLException {
-        return  null;
+        return dataDAO.readAllData();
     }
+
 }
