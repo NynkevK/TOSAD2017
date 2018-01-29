c04d7bcaf3157d9625c47310ac3ef53003349cab
Meimeifeng <tickets_to_life@hotmail.com>
2018-01-29 13:34:14 +0100

    aanpassing namen databasedao aanpassen

--- src/main/java/nl/hu/tosad2017/persistence/data/DatabaseDAO.java
+++ src/main/java/nl/hu/tosad2017/persistence/data/DatabaseDAO.java
@@ -18,11 +18,11 @@ public class DatabaseDAO extends TargetBaseDAO {
         Connection connection = super.getConn();
         String query = "SELECT table_name, column_name, data_type\n" +
                 "FROM user_tab_columns\n" +
-                "where   table_name= UPPER('KLANTEN')    and not data_type = UPPER('DATE')\n" +
-                "    or  table_name= UPPER('LEVERINGEN') and not data_type = UPPER('DATE') \n" +
-                "    or  table_name= UPPER('ORDERS')     and not data_type = UPPER('DATE')\n" +
-                "    or  table_name= UPPER('PRODUCTEN')  and not data_type = UPPER('DATE')\n" +
-                "    or  table_name= UPPER('LOCATIES')   and not data_type = UPPER('DATE')";
+                "where   table_name= UPPER('VBMG_KLANTEN')    and not data_type = UPPER('DATE')\n" +
+                "    or  table_name= UPPER('VBMG_LEVERINGEN') and not data_type = UPPER('DATE') \n" +
+                "    or  table_name= UPPER('VBMG_ORDERS')     and not data_type = UPPER('DATE')\n" +
+                "    or  table_name= UPPER('VBMG_PRODUCTEN')  and not data_type = UPPER('DATE')\n" +
+                "    or  table_name= UPPER('VBMG_LOCATIES')   and not data_type = UPPER('DATE')";
 
         PreparedStatement ps = connection.prepareStatement(query);
         ResultSet rs = ps.executeQuery(query);
