7257292cdfed657ea23bd6bd1e084c9130332277
MarkLanser <markklanser@gmail.com>
2018-01-30 12:43:54 +0100

    Resolved conflicts

--- src/main/java/nl/hu/tosad2017/webservices/GenerateResource.java
+++ src/main/java/nl/hu/tosad2017/webservices/GenerateResource.java
@@ -2,10 +2,8 @@ package nl.hu.tosad2017.webservices;
 
 import java.sql.SQLException;
 
-import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 
@@ -17,20 +15,12 @@ public class GenerateResource {
 	
 	@POST
 	@Produces("application/json")
-<<<<<<< HEAD
 	public String generateRule(@QueryParam("id") String id,
-							@QueryParam("ruletype") String ruletype) 
-							//@QueryParam("otherTable") String otherTable,
-							//@QueryParam("otherColumn") String otherColumn; 
-							throws SQLException {
-=======
-	public String generateRule(@PathParam("id") String id,
-								@PathParam("type") String type) throws SQLException {
+								@QueryParam("type") String type) throws SQLException {
 				
->>>>>>> ae9b95ca3cb9b93a4a9a25c829f1ef9e01c86c72
 		Integer idInt = Integer.parseInt(id);
 		
-		service.generateRule(idInt, ruletype);
+		service.generateRule(idInt, type);
 		
 		return "success";
 	}
