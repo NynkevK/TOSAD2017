aec339f601d215f88c10d405de6b848edee8d5c1
Ezri-Mudde <ezri.mudde@student.hu.nl>
2018-01-31 16:33:21 +0100

    fixed method name and param name

--- src/main/java/nl/hu/tosad2017/webservices/OtherRuleResource.java
+++ src/main/java/nl/hu/tosad2017/webservices/OtherRuleResource.java
@@ -84,7 +84,7 @@ public class OtherRuleResource {
 
 		@POST
 		@Produces("application/json")
-		public Response defineRangeRule(
+		public Response defineOtherRule(
 									@QueryParam("code") String code,
 									@QueryParam("name") String name,
 									@QueryParam("message") String message,
@@ -92,7 +92,7 @@ public class OtherRuleResource {
 									@QueryParam("columnName") String columnName,
 									@QueryParam("columnType") String columnType,
 									@QueryParam("otherColumn") String otherColumn,
-									@QueryParam("otherColumn") String otherTable,
+									@QueryParam("otherTable") String otherTable,
 									@QueryParam("table") String table,
 									@QueryParam("status") String status,
 									@QueryParam("operator") String operator,
